76ed8a447aec0663919caea7db2cca93d4e9b5ca
Abhiram <abhi172993@gmail.com>
2026-07-01 07:38:03 +0530

    Remove all files except frontend

--- rest_blog/blogapp/urls.py
+++
@@ -1,9 +0,0 @@
-
-from .views import create_blog,view_blog,edit_blog,delete_blog,user_login,register_login,view_blog_user
-from django.urls import path
-urlpatterns = [
-   path('',view_blog),
-   path('create/',create_blog),
-   path('edit/<int:id>/',edit_blog),
-   path('delete/<int:id>/',delete_blog),
-]
